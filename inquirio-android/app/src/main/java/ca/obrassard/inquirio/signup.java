064c12bdbbe6d24ad323628cda0f934f6cb01d63
Olivier Brassard <brassard.oli@gmail.com>
2018-08-28 15:19:22 -0400

    safety commit before refactoring

---
+++ inquirio-android/app/src/main/java/ca/obrassard/inquirio/signup.java
@@ -0,0 +1,14 @@
+package ca.obrassard.inquirio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
+
