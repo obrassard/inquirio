3c4e47655e38fac603d59704c65c70bc3c8e4040
Olivier Brassard <brassard.oli@gmail.com>
2018-08-30 23:59:16 -0400

    Intégration du place picker api

--- inquirio-android/app/src/main/java/ca/obrassard/inquirio/AddItemActivity.java
+++ inquirio-android/app/src/main/java/ca/obrassard/inquirio/AddItemActivity.java
@@ -1,8 +1,10 @@
 package ca.obrassard.inquirio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +14,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 public class AddItemActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    TextView txtSelectedLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +44,41 @@ public class AddItemActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Intialisation des vues personnalisées
+        txtSelectedLocation = findViewById(R.id.selectedlocation);
+
+        txtSelectedLocation.setText("");
+
+        Button btn_setLocation = findViewById(R.id.btn_addlocation);
+        btn_setLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getLocationWithPlacePicker();
+            }
+        });
+    }
+
+    public void getLocationWithPlacePicker(){
+        int PLACE_PICKER_REQUEST = 1;
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        try {
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            Log.e("MAP", "GooglePlayServicesRepairableException : "+ e.getMessage());
+        } catch (GooglePlayServicesNotAvailableException e) {
+            Log.e("MAP", "GooglePlayServicesNotAvailableException : "+ e.getMessage());
+        }
+    }
+
+    //Recuperation de l'emplacement séléctionné dans le placepicker
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                txtSelectedLocation.setText(place.getName());
+            }
+        }
     }
 
     @Override
