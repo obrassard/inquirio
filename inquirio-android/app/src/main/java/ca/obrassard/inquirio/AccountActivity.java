cfccb0f54ebcd1a1d4ddce5066b856e727dafb4a
Olivier Brassard <brassard.oli@gmail.com>
2018-08-31 20:53:47 -0400

    modif a AccountActivity

--- inquirio-android/app/src/main/java/ca/obrassard/inquirio/AccountActivity.java
+++ inquirio-android/app/src/main/java/ca/obrassard/inquirio/AccountActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class AccountActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -35,6 +36,19 @@ public class AccountActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         //endregion
+
+        Button btn_logout = findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+    }
+
+    private void logout (){
+        Intent i = new Intent(this,LoginHomeActivity.class);
+        startActivity(i);
     }
 
     //region [Evennement du tirroir]
