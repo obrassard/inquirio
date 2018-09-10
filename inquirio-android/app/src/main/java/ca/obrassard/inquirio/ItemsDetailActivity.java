f6af3b2117d582af744238b2b12f08a78d51ba5d
Olivier Brassard <brassard.oli@gmail.com>
2018-09-09 22:37:06 -0400

    Ajout d'une plus grosse carte google map et config du mapview

--- inquirio-android/app/src/main/java/ca/obrassard/inquirio/ItemsDetailActivity.java
+++ inquirio-android/app/src/main/java/ca/obrassard/inquirio/ItemsDetailActivity.java
@@ -14,8 +14,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class ItemsDetailActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+
+    private MapView mapView;
+    private static final String MAP_VIEW_BUNDLE_KEY = "MAPKEY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +35,96 @@ public class ItemsDetailActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        }
+
+        mapView = findViewById(R.id.map);
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
 
         //endregion
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        googleMap.setMinZoomPreference(16);
+        final LatLng edouard = new LatLng(45.536286, -73.493004);
+        googleMap.addMarker(new MarkerOptions().position(edouard));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(edouard));
+
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                Intent i = new Intent(getBaseContext(),MapDetail.class);
+                i.putExtra("lat",edouard.latitude);
+                i.putExtra("lng",edouard.longitude);
+                startActivity(i);
+            }
+        });
+
+
+
+    }
+
+    //region [Override des methodes de l'activité pour le mapview]
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+        }
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    //endregion
 
     //region [Evennement du tirroir]
     @SuppressWarnings("StatementWithEmptyBody")
